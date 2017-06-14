03e370b1d820953fc6d13c7d696d75e3e605cf1c
Florian Klemme <kruecke@kruecke.net>
2017-06-14 14:34:33 +0200

    Updated projects.

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -2,10 +2,16 @@
 
 #include "KBot.h"
 
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN
+// Windows Header Files
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
-            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
             break;
     }
@@ -17,5 +23,5 @@ extern "C" __declspec(dllexport) void gameInit(BWAPI::Game *game) {
 }
 
 extern "C" __declspec(dllexport) BWAPI::AIModule *newAIModule() {
-    return new KBot::KBot();
+    return new KBot::KBot;
 }
