664def50681f32f0f806cb652ab72cc122cf13a2
Florian Klemme <kruecke@kruecke.net>
2017-06-16 13:17:57 +0200

    Build debug and release.

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 :: Build BWAPI
 pushd %CD%
 cd bwapi\bwapi
-::msbuild /p:Configuration=Debug /p:Platform=Win32 /target:BWAPILIB bwapi.sln
+msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPILIB bwapi.sln
 msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPILIB bwapi.sln
 popd
 
 :: Build KBot
-::msbuild /p:Configuration=Debug /p:Platform=x86 KBot.sln
+msbuild /p:Configuration=Debug   /p:Platform=x86 KBot.sln
 msbuild /p:Configuration=Release /p:Platform=x86 KBot.sln
