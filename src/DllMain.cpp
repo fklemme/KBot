354037811bef1a1202ddf7f00a4f7a2537fbd887
Florian Klemme <kruecke@kruecke.net>
2017-06-16 17:04:09 +0200

    Removed comment.

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -4,7 +4,6 @@
 
 // Exclude rarely-used stuff from Windows headers
 #define WIN32_LEAN_AND_MEAN
-// Windows Header Files
 #include <windows.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
