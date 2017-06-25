c9726c5ce788835355edf7739362bd1741fe6ba9
Florian Klemme <kruecke@kruecke.net>
2017-06-25 22:01:25 +0200

    Minor changes.

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,5 @@
+// A collection of useful helper functions.
+
 #pragma once
 
 #include <BWAPI.h>
@@ -6,8 +8,8 @@
 namespace KBot {
 
     // Returns distance between positions considering BWEM paths. Returns max. int if no path is available.
-    template <typename PositionTypeA, typename PositionTypeB>
-    int distance(const PositionTypeA &a, const PositionTypeB &b, BWEM::Map &map = BWEM::Map::Instance()) {
+    template <typename PositionA, typename PositionB>
+    int distance(const PositionA &a, const PositionB &b, BWEM::Map &map = BWEM::Map::Instance()) {
         int length;
         map.GetPath(BWAPI::Position(a), BWAPI::Position(b), &length);
         return length != -1 ? length : std::numeric_limits<int>::max();
