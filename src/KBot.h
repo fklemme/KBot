02985906fd805ae19850068732b5caa615930976
Florian Klemme <kruecke@kruecke.net>
2017-06-26 03:19:12 +0200

    Removed redundant virtual keyword.

--- src/KBot.h
+++ src/KBot.h
@@ -20,23 +20,23 @@ namespace KBot {
         KBot &operator=(KBot&&) = delete;
 
         // Implement BWAPI interface.
-        virtual void onStart() override;
-        virtual void onEnd(bool isWinner) override;
-        virtual void onFrame() override;
-        virtual void onSendText(std::string text) override;
-        virtual void onReceiveText(BWAPI::Player player, std::string text) override;
-        virtual void onPlayerLeft(BWAPI::Player player) override;
-        virtual void onNukeDetect(BWAPI::Position target) override;
-        virtual void onUnitDiscover(BWAPI::Unit unit) override;
-        virtual void onUnitEvade(BWAPI::Unit unit) override;
-        virtual void onUnitShow(BWAPI::Unit unit) override;
-        virtual void onUnitHide(BWAPI::Unit unit) override;
-        virtual void onUnitCreate(BWAPI::Unit unit) override;
-        virtual void onUnitDestroy(BWAPI::Unit unit) override;
-        virtual void onUnitMorph(BWAPI::Unit unit) override;
-        virtual void onUnitRenegade(BWAPI::Unit unit) override;
-        virtual void onSaveGame(std::string gameName) override;
-        virtual void onUnitComplete(BWAPI::Unit unit) override;
+        void onStart() override;
+        void onEnd(bool isWinner) override;
+        void onFrame() override;
+        void onSendText(std::string text) override;
+        void onReceiveText(BWAPI::Player player, std::string text) override;
+        void onPlayerLeft(BWAPI::Player player) override;
+        void onNukeDetect(BWAPI::Position target) override;
+        void onUnitDiscover(BWAPI::Unit unit) override;
+        void onUnitEvade(BWAPI::Unit unit) override;
+        void onUnitShow(BWAPI::Unit unit) override;
+        void onUnitHide(BWAPI::Unit unit) override;
+        void onUnitCreate(BWAPI::Unit unit) override;
+        void onUnitDestroy(BWAPI::Unit unit) override;
+        void onUnitMorph(BWAPI::Unit unit) override;
+        void onUnitRenegade(BWAPI::Unit unit) override;
+        void onSaveGame(std::string gameName) override;
+        void onUnitComplete(BWAPI::Unit unit) override;
 
         // Getter for members.
         Manager &manager() { return m_manager; }
