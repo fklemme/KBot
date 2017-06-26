c8b7e103e6970cdeb1c07d0318a947bad8fc3bfe
Florian Klemme <mail@florianklemme.de>
2017-06-26 07:00:53 +0200

    Applied clang-format.

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -1,5 +1,5 @@
-#include <BWAPI.h>
 #include "KBot.h"
+#include <BWAPI.h>
 
 // Exclude rarely-used stuff from Windows headers
 #define WIN32_LEAN_AND_MEAN
@@ -7,19 +7,15 @@
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
-        case DLL_PROCESS_ATTACH:
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
-        case DLL_PROCESS_DETACH:
-            break;
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
     }
     return TRUE;
 }
 
-extern "C" __declspec(dllexport) void gameInit(BWAPI::Game *game) {
-    BWAPI::BroodwarPtr = game;
-}
+extern "C" __declspec(dllexport) void gameInit(BWAPI::Game *game) { BWAPI::BroodwarPtr = game; }
 
-extern "C" __declspec(dllexport) BWAPI::AIModule *newAIModule() {
-    return new KBot::KBot;
-}
+extern "C" __declspec(dllexport) BWAPI::AIModule *newAIModule() { return new KBot::KBot; }
