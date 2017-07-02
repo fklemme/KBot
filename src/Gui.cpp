f40161a9e39f946b2a042eefc707bd15a7adf742
Florian Klemme <kruecke@kruecke.net>
2017-07-02 23:09:46 +0200

    Set up simple gui structure.

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -1,43 +1,50 @@
 #include "Gui.h"
 
-#include <nana/gui.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/label.hpp>
 #include <thread>
 
 namespace KBot {
 
-static void nanaHelloWorld() {
-    using namespace nana;
+Gui::Gui(const KBot &kbot) : m_kbot(kbot) {
+    // Open gui in a new thread.
+    m_guiThread = std::make_unique<std::thread>([this]() {
+        // All gui elements have to be created by the thread that runs nana::exec().
+        nana::form form;    // main form
+        m_mainForm = &form; // store reference in Gui
 
-    // Define a form.
-    form fm;
+        // Define a label and display a text.
+        nana::label lab{form, "Hello, <bold blue size=16>Nana C++ Library</>"};
+        lab.format(true);
 
-    // Define a label and display a text.
-    label lab{fm, "Hello, <bold blue size=16>Nana C++ Library</>"};
-    lab.format(true);
+        // Define a button and answer the click event.
+        nana::button btn{form, "Quit"};
+        btn.events().click([&form] { form.close(); });
 
-    // Define a button and answer the click event.
-    button btn{fm, "Quit"};
-    btn.events().click([&fm] { fm.close(); });
+        // Layout management
+        form.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+        form["text"] << lab;
+        form["button"] << btn;
+        form.collocate();
 
-    // Layout management
-    fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
-    fm["text"] << lab;
-    fm["button"] << btn;
-    fm.collocate();
+        // Show the form
+        form.show();
+        std::cout << "Gui opened!" << std::endl; // DEBUG
 
-    // Show the form
-    fm.show();
+        // Start to event loop process, it blocks until the form is closed.
+        nana::exec();
+        std::cout << "Gui closed!" << std::endl; // DEBUG
 
-    // Start to event loop process, it blocks until the form is closed.
-    exec();
+        // Main form is about to be destroyed. Clear reference.
+        m_mainForm = nullptr;
+    });
 }
 
-Gui::Gui(const KBot &kbot) : m_kbot(kbot) {
-    // Just for testing...
-    std::thread gui(nanaHelloWorld);
-    gui.detach();
+Gui::~Gui() {
+    if (m_mainForm)
+        m_mainForm->close();
+    if (m_guiThread)
+        m_guiThread->join();
 }
 
 void Gui::update() {
--- src/Gui.h
+++ src/Gui.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "KBot.h"
+#include <memory>
+#include <nana/gui.hpp>
+#include <thread>
 
 namespace KBot {
 
 class Gui {
 public:
     Gui(const KBot &kbot);
+    ~Gui();
 
     void update();
 
 private:
-    const KBot &m_kbot;
+    const KBot &                 m_kbot;
+    nana::form *                 m_mainForm = nullptr; // FIXME: Protect with mutex!
+    std::unique_ptr<std::thread> m_guiThread;
 };
 
 } // namespace KBot
